5f442443791c7e4454878d175a8bc278e7311b61
b-kkoo <bmcow331@naver.com>
2024-11-01 14:19:48 +0900

    remove: Remove QuarryNPC.cs and Folder

--- Assets/1. Scripts/ScriptableObject/NPC/QuarryNPC/QuarryNPC.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Quarry_", menuName = "NPCBase/QuarryNPC", order = 0)]
-public class QuarryNPC : NPCBase
-{
-
-}
