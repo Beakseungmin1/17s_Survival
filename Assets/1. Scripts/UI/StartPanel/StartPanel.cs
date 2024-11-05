11a00c76c32063e24a62ffd15c94bf2f7433d079
simple2126 <kimcw212@gmail.com>
2024-11-05 22:20:33 +0900

    feat: add StartScene

--- Assets/1. Scripts/UI/StartPanel/StartPanel.cs
+++ Assets/1. Scripts/UI/StartPanel/StartPanel.cs
@@ -1,18 +1,45 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StartPanel : MonoBehaviour
 {
     public GameObject startPanel;
-    public GameObject gameDescription;
+    public GameObject Control1;
+    public GameObject Control2;
 
     public void ClickGameStart()
     {
         startPanel.SetActive(false);
+        SceneManager.LoadScene("SampleScene");
     }
 
-    public void ClickGameDescription()
+    public void ClickControlsDescriptionButton()
     {
-        gameDescription.SetActive(true);
+        if (Control1.activeInHierarchy == false)
+            Control1.SetActive(true);
     }
 
+    public void ClickRightArrowButton()
+    {
+        if (Control2.activeInHierarchy == false)
+        {
+            Control1.SetActive(false);
+            Control2.SetActive(true);
+        }
+    }
+
+    public void ClickLeftArrowButton()
+    {
+        if (Control1.activeInHierarchy == false)
+        {
+            Control2.SetActive(false);
+            Control1.SetActive(true);
+        }
+    }
+
+    public void ClickExitButton()
+    {
+        Control1.SetActive(false);
+        Control2.SetActive(false);
+    }
 }
